3625f8c0a6c8e8a8745a0d32f20c7c7afbd17849
Henrique Sander Fernandes <hqsander@gmail.com>
2019-12-28 14:23:23 -0300

    Commit inicial

---
+++ src/main/java/com/gestaoprotese/sigpro/SigproApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoprotese.sigpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaoprotese/sigpro/SigproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoprotese.sigpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
