6aead71da9f343913c189266e83b11ea7fb5a818
Henrique Sander Fernandes <hqsander@gmail.com>
2019-12-31 16:36:39 -0200

    Implementa GET Instituicao com paginação e parâmetros opcionais

--- src/main/java/com/gestaoprotese/sigpro/resources/InstituicaoResource.java
+++ src/main/java/com/gestaoprotese/sigpro/resources/InstituicaoResource.java
@@ -7,11 +7,13 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -60,6 +62,17 @@ public class InstituicaoResource {
 	public ResponseEntity<?> findAll() {
 		List<Instituicao> list = service.findAll();
 		List<InstituicaoDTO> listDto = list.stream().map(obj -> new InstituicaoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<?> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Instituicao> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<InstituicaoDTO> listDto = list.map(obj -> new InstituicaoDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
 }
